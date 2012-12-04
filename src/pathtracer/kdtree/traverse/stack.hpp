1ae993de1dd39b07612b6a36161f7cfa405ad727
daoo <daoo314@gmail.com>
2012-12-04 22:23:44 +0100

    Remove state

--- src/pathtracer/kdtree/traverse/stack.hpp
+++ src/pathtracer/kdtree/traverse/stack.hpp
@@ -14,37 +14,29 @@ namespace kdtree {
   namespace traverse {
     namespace detail {
       template <typename Iter>
-      struct State {
-        State(math::Ray& r, Intersection& i) : stack(), ray(r), isect(i) { }
-
-        std::stack<std::tuple<Iter, float, float>> stack;
-        math::Ray& ray;
-        Intersection& isect;
-      };
-
-      template <typename Iter>
-      bool searchNode(State<Iter>& state, Iter iter, float mint, float maxt) {
+      bool searchNode(Iter iter, std::stack<std::tuple<Iter, float, float>> stack,
+          float mint, float maxt, math::Ray& ray, Intersection& isect) {
         if (iter.isLeaf()) {
           bool hit = false;
 
           for (const Triangle* tri : iter.triangles()) {
-            hit |= intersects(*tri, state.ray, state.isect);
+            hit |= intersects(*tri, ray, isect);
           }
 
-          if (hit && state.ray.maxt < maxt) {
+          if (hit && ray.maxt < maxt) {
             return true;
-          } else if (state.stack.empty()) {
+          } else if (stack.empty()) {
             return false;
           } else {
-            std::tuple<Iter, float, float> tmp = state.stack.top();
-            state.stack.pop();
-            return searchNode(state, std::get<0>(tmp), std::get<1>(tmp), std::get<2>(tmp));
+            std::tuple<Iter, float, float> tmp = stack.top();
+            stack.pop();
+            return searchNode(std::get<0>(tmp), stack, std::get<1>(tmp), std::get<2>(tmp), ray, isect);
           }
         } else if (iter.isSplit()) {
           float p = iter.split();
 
-          float o = helpers::swizzle(state.ray.origin, iter.axis());
-          float d = helpers::swizzle(state.ray.direction, iter.axis());
+          float o = helpers::swizzle(ray.origin, iter.axis());
+          float d = helpers::swizzle(ray.direction, iter.axis());
 
           float t = (p - o) / d;
 
@@ -53,12 +45,12 @@ namespace kdtree {
           helpers::order(d, iter.left(), iter.right(), first, second);
 
           if (t >= maxt) {
-            return searchNode(state, first, mint, maxt);
+            return searchNode(first, stack, mint, maxt, ray, isect);
           } else if (t <= mint) {
-            return searchNode(state, second, mint, maxt);
+            return searchNode(second, stack, mint, maxt, ray, isect);
           } else {
-            state.stack.push(std::make_tuple(second, t, maxt));
-            return searchNode(state, first, mint, t);
+            stack.push(std::make_tuple(second, t, maxt));
+            return searchNode(first, stack, mint, t, ray, isect);
           }
         }
 
@@ -69,8 +61,9 @@ namespace kdtree {
 
     template <typename Tree>
     bool stackSearchTree(const Tree& tree, math::Ray& ray, Intersection& isect) {
-      detail::State<typename Tree::Iterator> state(ray, isect);
-      return detail::searchNode(state, typename Tree::Iterator(tree), ray.mint, ray.maxt);
+      return detail::searchNode(typename Tree::Iterator(tree),
+          std::stack<std::tuple<typename Tree::Iterator, float, float>>(),
+          ray.mint, ray.maxt, ray, isect);
     }
   }
 }
