a431d09b5479fb5e4e9fb74454ea68ad6d9aee25
daoo <daoo314@gmail.com>
2012-12-04 22:41:23 +0100

    Use the restart algorithm instead

--- src/pathtracer/kdtree/intersections.cpp
+++ src/pathtracer/kdtree/intersections.cpp
@@ -1,15 +1,15 @@
 #include "intersections.hpp"
 
-#include "kdtree/traverse/stack.hpp"
+#include "kdtree/traverse/restart.hpp"
 
 namespace kdtree {
   bool intersects(const KdTreeLinked& tree, math::Ray& ray, Intersection& isect) {
-    return traverse::stackSearchTree<KdTreeLinked>(tree, ray, isect);
+    return traverse::restartSearchTree<KdTreeLinked>(tree, ray, isect);
   }
 
   bool intersects(const KdTreeLinked& tree, const math::Ray& ray) {
     Intersection isect;
     math::Ray raycopy(ray);
-    return traverse::stackSearchTree<KdTreeLinked>(tree, raycopy, isect);
+    return traverse::restartSearchTree<KdTreeLinked>(tree, raycopy, isect);
   }
 }
