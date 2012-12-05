1082f4035ba7a48b4b45dc76ba071713d71749f1
daoo <daoo314@gmail.com>
2012-12-05 15:31:47 +0100

    Templatize

--- src/pathtracer/kdtree/intersections.hpp
+++ src/pathtracer/kdtree/intersections.hpp
@@ -1,12 +1,21 @@
 #ifndef INTERSECTIONS_HPP_LOZBUIUH
 #define INTERSECTIONS_HPP_LOZBUIUH
 
-#include "kdtree/linked.hpp"
+#include "kdtree/traverse/restart.hpp"
 #include "math/ray.hpp"
 
 namespace kdtree {
-  bool intersects(const KdTreeLinked& tree, math::Ray& ray, Intersection& isect);
-  bool intersects(const KdTreeLinked& tree, const math::Ray& ray);
+  template <typename Tree>
+  bool intersects(const Tree& tree, math::Ray& ray, Intersection& isect) {
+    return traverse::restartSearchTree<KdTreeLinked>(tree, ray, isect);
+  }
+
+  template <typename Tree>
+  bool intersects(const Tree& tree, const math::Ray& ray) {
+    Intersection isect;
+    math::Ray raycopy(ray);
+    return traverse::restartSearchTree<KdTreeLinked>(tree, raycopy, isect);
+  }
 }
 
 #endif /* end of include guard: INTERSECTIONS_HPP_LOZBUIUH */
