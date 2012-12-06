f699181bcd39ab00ae8ed2046180d0a210824bcb
daoo <daoo314@gmail.com>
2012-12-06 19:30:24 +0100

    Refactor

--- src/pathtracer/kdtree/tree.hpp
+++ src/pathtracer/kdtree/tree.hpp
@@ -1,9 +1,9 @@
 #ifndef TREE_HPP_47RCBESP
 #define TREE_HPP_47RCBESP
 
-#include "kdtree/build/halfsplits.hpp"
+#include "kdtree/build.hpp"
 #include "kdtree/dt/linked.hpp"
-#include "kdtree/traverse/restart.hpp"
+#include "kdtree/traverse.hpp"
 #include "math/ray.hpp"
 
 namespace kdtree {
@@ -11,20 +11,20 @@ namespace kdtree {
 
   template <typename Tree>
   void buildTree(Tree& iter, const std::vector<Triangle>& triangles) {
-    build::halfSplitsBuildTree(typename Tree::BuildIter(iter),
+    buildTree(typename Tree::BuildIter(iter),
         helpers::findBounding(triangles), triangles);
   }
 
   template <typename Tree>
   bool intersects(const Tree& tree, math::Ray& ray, Intersection& isect) {
-    return traverse::restartSearchTree<Tree>(tree, ray, isect);
+    return searchTree<Tree>(tree, ray, isect);
   }
 
   template <typename Tree>
   bool intersects(const Tree& tree, const math::Ray& ray) {
     Intersection isect;
     math::Ray raycopy(ray);
-    return traverse::restartSearchTree<Tree>(tree, raycopy, isect);
+    return searchTree<Tree>(tree, raycopy, isect);
   }
 }
 
