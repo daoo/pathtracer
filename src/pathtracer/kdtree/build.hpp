50b089d7f68dcbf8835f46a2d3e0dccc03a586ed
daoo <daoo314@gmail.com>
2012-12-07 13:49:49 +0100

    Fix building tree

--- src/pathtracer/kdtree/build.hpp
+++ src/pathtracer/kdtree/build.hpp
@@ -11,27 +11,12 @@
 
 namespace kdtree {
   template <typename Iter>
-  void buildTree(Iter iter, math::Aabb bounding, const std::vector<Triangle>& triangles) {
-    if (iter.depth() >= 10 || triangles.size() <= 5) {
+  void buildTree(Iter iter, const math::Aabb& bounding, const std::vector<Triangle>& triangles) {
+    if (iter.depth() >= 12 || triangles.size() <= 10) {
       iter.leaf(triangles);
     } else {
-      float d = helpers::swizzle(bounding.center, iter.axis());
-
-      std::vector<Triangle> left_triangles, right_triangles;
-      for (const Triangle& tri : triangles) {
-        if (helpers::containsLeft(tri, d, iter.axis())) {
-          left_triangles.push_back(tri);
-        }
-
-        if (helpers::containsRight(tri, d, iter.axis())) {
-          right_triangles.push_back(tri);
-        }
-      }
-
-      assert(left_triangles.size() + right_triangles.size() >= triangles.size()
-          && "geometry has disappeared");
-
-      glm::vec3 new_size = bounding.half / 2.0f;
+      glm::vec3 new_size = bounding.half;
+      helpers::swizzle(new_size, iter.axis()) = helpers::swizzle(bounding.half / 2.0f, iter.axis());
 
       math::Aabb left_bounding({ bounding.center, new_size });
       math::Aabb right_bounding({ bounding.center, new_size });
@@ -39,6 +24,21 @@ namespace kdtree {
       helpers::swizzle(left_bounding.center, iter.axis())  -= helpers::swizzle(new_size, iter.axis());
       helpers::swizzle(right_bounding.center, iter.axis()) += helpers::swizzle(new_size, iter.axis());
 
+      std::vector<Triangle> left_triangles, right_triangles;
+      for (const Triangle& tri : triangles) {
+        if (triBoxOverlap(left_bounding, tri.v0, tri.v1, tri.v2)) {
+          left_triangles.push_back(tri);
+        }
+
+        if (triBoxOverlap(right_bounding, tri.v0, tri.v1, tri.v2)) {
+          right_triangles.push_back(tri);
+        }
+      }
+
+      assert(left_triangles.size() + right_triangles.size() >= triangles.size()
+          && "geometry has disappeared");
+
+      float d = helpers::swizzle(bounding.center, iter.axis());
       iter.split(d);
 
       buildTree(iter.left(), left_bounding, left_triangles);
