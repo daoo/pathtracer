5e36f7462e9ce973879605913e8c26275c3dcfc6
daoo <daoo314@gmail.com>
2012-12-08 16:35:49 +0100

    Use a unsigned int for index

--- src/pathtracer/kdtree/dt/array.hpp
+++ src/pathtracer/kdtree/dt/array.hpp
@@ -23,30 +23,30 @@ namespace kdtree {
           ~Node() { }
 
           Node() : m_data(EMPTY_LEAF) { }
-          Node(int32_t index) : m_data((index << 1) & MASK_DATA) { }
+          Node(uint32_t index) : m_data((index << 1) & MASK_DATA) { }
           Node(float distance) : m_distance(distance) { m_data |= MASK_TYPE; }
 
           bool isLeaf() const { return (m_data & MASK_TYPE) == TYPE_LEAF; }
           bool isSplit() const { return (m_data & MASK_TYPE) == TYPE_SPLIT; }
 
-          int32_t getIndex() const { return m_data >> 1; }
+          uint32_t getIndex() const { return m_data >> 1; }
           float getDistance() const { return m_distance; }
 
           bool hasTriangles() const { return m_data != EMPTY_LEAF; }
 
         private:
           union {
-            int32_t m_data;
+            uint32_t m_data;
             float m_distance;
           };
 
-          static constexpr int32_t MASK_TYPE = 0x1;
-          static constexpr int32_t MASK_DATA = ~MASK_TYPE;
+          static constexpr uint32_t MASK_TYPE = 0x1;
+          static constexpr uint32_t MASK_DATA = ~MASK_TYPE;
 
-          static constexpr int32_t EMPTY_LEAF = std::numeric_limits<int32_t>::max() & MASK_DATA;
+          static constexpr uint32_t EMPTY_LEAF = std::numeric_limits<uint32_t>::max() & MASK_DATA;
 
-          static constexpr int32_t TYPE_LEAF = 0;
-          static constexpr int32_t TYPE_SPLIT = 1;
+          static constexpr uint32_t TYPE_LEAF = 0;
+          static constexpr uint32_t TYPE_SPLIT = 1;
       };
 
       class BuildIter {
@@ -72,7 +72,7 @@ namespace kdtree {
               setNode(Node());
             } else {
               m_tree.m_leaf_store.push_back(std::vector<Triangle>(triangles));
-              setNode(Node(static_cast<int32_t>(m_tree.m_leaf_store.size() - 1)));
+              setNode(Node(static_cast<uint32_t>(m_tree.m_leaf_store.size() - 1)));
             }
           }
 
