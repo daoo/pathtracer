69f583bc3e538515322cbd530c2507caa714f7b2
daoo <daoo314@gmail.com>
2012-12-08 12:35:39 +0100

    Use array

--- src/pathtracer/kdtree/tree.hpp
+++ src/pathtracer/kdtree/tree.hpp
@@ -2,16 +2,17 @@
 #define TREE_HPP_47RCBESP
 
 #include "pathtracer/kdtree/build.hpp"
+#include "pathtracer/kdtree/dt/array.hpp"
 #include "pathtracer/kdtree/dt/linked.hpp"
 #include "pathtracer/kdtree/traverse.hpp"
 #include "pathtracer/math/ray.hpp"
 
 namespace kdtree {
-  typedef KdTreeLinked KdTree;
+  typedef KdTreeArray KdTree;
 
   template <typename Tree>
-  void buildTree(Tree& iter, const std::vector<Triangle>& triangles) {
-    buildTree(typename Tree::BuildIter(iter),
+  void buildTree(Tree& tree, const std::vector<Triangle>& triangles) {
+    buildTree(typename Tree::BuildIter(tree),
         helpers::findBounding(triangles), triangles);
   }
 
