2c5f2e8dce9b3400b4a612d223f5edb946e52927
daoo <daoo314@gmail.com>
2012-12-08 14:24:30 +0100

    Fix leaf triangle test

--- src/pathtracer/kdtree/dt/array.hpp
+++ src/pathtracer/kdtree/dt/array.hpp
@@ -20,9 +20,9 @@ namespace kdtree {
         public:
           enum NodeType { Split, Leaf };
 
-          Node() { }
           ~Node() { }
 
+          Node() : m_data(EMPTY_LEAF) { }
           Node(int32_t index) : m_data((index << 1) & MASK_DATA) { }
           Node(float distance) : m_distance(distance) { m_data |= MASK_TYPE; }
 
@@ -32,6 +32,8 @@ namespace kdtree {
           int32_t getIndex() const { return m_data >> 1; }
           float getDistance() const { return m_distance; }
 
+          bool hasTriangles() const { return m_data == EMPTY_LEAF; }
+
         private:
           union {
             int32_t m_data;
@@ -40,6 +42,8 @@ namespace kdtree {
 
           static constexpr int32_t MASK_TYPE = 0x1;
           static constexpr int32_t MASK_DATA = ~MASK_TYPE;
+
+          static constexpr int32_t EMPTY_LEAF = std::numeric_limits<int32_t>::max() & MASK_TYPE;
 
           static constexpr int32_t TYPE_LEAF = 0;
           static constexpr int32_t TYPE_SPLIT = 1;
@@ -128,7 +132,7 @@ namespace kdtree {
 
           bool hasTriangles() const {
             assert(isLeaf());
-            return m_node->getIndex() != std::numeric_limits<int32_t>::max();
+            return m_node->hasTriangles();
           }
 
           bool isSplit() const {
