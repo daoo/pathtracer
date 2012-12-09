161f8efa9873fb0af97b1332c6e3fb65f4838eb5
daoo <daoo314@gmail.com>
2012-12-09 12:55:37 +0100

    More flexibel MT

--- src/cmdline/main.cpp
+++ src/cmdline/main.cpp
@@ -10,6 +10,8 @@ using namespace std::chrono;
 using namespace std;
 
 void trace(Pathtracer& pt, size_t samples) {
+  assert(samples > 0);
+
   util::Clock clock;
   while (pt.samples() < samples) {
     clock.start();
@@ -21,47 +23,51 @@ void trace(Pathtracer& pt, size_t samples) {
   }
 }
 
-void program(const string& objFile, const string& output, size_t w, size_t h, size_t camera, size_t samples) {
+void program(const string& objFile, const string& outFile, size_t w, size_t h,
+    size_t camera, size_t sampleCount, size_t threadCount) {
+  assert(!objFile.empty());
+  assert(!outFile.empty());
+  assert(w > 0 && h > 0);
+  assert(threadCount > 0);
+  assert(sampleCount > 0);
+
   OBJModel model;
   model.load(objFile);
 
   const Scene scene(model);
 
-  Pathtracer pt1(w, h, scene, camera);
-  Pathtracer pt2(w, h, scene, camera);
-  Pathtracer pt3(w, h, scene, camera);
-  Pathtracer pt4(w, h, scene, camera);
+  size_t samples_thread = sampleCount / threadCount;
 
-  thread t1(trace, ref(pt1), samples / 4);
-  thread t2(trace, ref(pt2), samples / 4);
-  thread t3(trace, ref(pt3), samples / 4);
-  thread t4(trace, ref(pt4), samples / 4);
+  std::vector<Pathtracer> tracers;
+  std::vector<thread> threads;
+  for (size_t i = 0; i < threadCount; ++i) {
+    tracers.push_back(Pathtracer(w, h, scene, camera));
+    threads.push_back(thread(trace, ref(tracers.back()), samples_thread));
+  }
 
-  t1.join();
-  t2.join();
-  t3.join();
-  t4.join();
+  for (thread& th : threads) {
+    th.join();
+  }
 
   util::SampleBuffer result(w, h);
-  result.append(pt1.buffer());
-  result.append(pt2.buffer());
-  result.append(pt3.buffer());
-  result.append(pt4.buffer());
-
-  util::writeImage(output, result);
+  for (const Pathtracer& tracer : tracers) {
+    result.append(tracer.buffer());
+  }
+  util::writeImage(outFile, result);
 }
 
 int main(int argc, char* argv[]) {
-  if (argc == 6) {
+  if (argc == 7) {
     string obj_file = argv[1];
     string img_file = argv[2];
     size_t width    = parse<size_t>(argv[3]);
     size_t height   = parse<size_t>(argv[4]);
     size_t samples  = parse<size_t>(argv[5]);
+    size_t threads  = parse<size_t>(argv[6]);
 
-    program(obj_file, img_file, width, height, 0, samples);
+    program(obj_file, img_file, width, height, 0, samples, threads);
   } else {
-    cout << "Usage: pathtracer model.obj output.png width height samples\n";
+    cout << "Usage: pathtracer model.obj output.png width height samples threads\n";
   }
 
   return 0;
