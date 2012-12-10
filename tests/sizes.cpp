5579670b24fdf3c46105c9d77baf40ee26b9f511
daoo <daoo314@gmail.com>
2012-12-10 10:10:00 +0100

    Some more sizes

--- tests/sizes.cpp
+++ tests/sizes.cpp
@@ -8,8 +8,11 @@ using namespace kdtree;
 using namespace std;
 
 int main() {
-  cout << "sizeof(KdTreeLinked::Node) = " << sizeof(KdTreeLinked::Node) << " bytes\n";
-  cout << "sizeof(KdTreeArray::Node) = " << sizeof(KdTreeArray::Node) << " bytes\n";
-  cout << "sizeof(KdTreeCompact::Node) = " << sizeof(KdTreeCompact::Node) << " bytes\n";
+  cout << "KdTreeLinked::Node\t\t"  << sizeof(KdTreeLinked::Node)  << " bytes\n";
+  cout << "KdTreeArray::Node\t\t"   << sizeof(KdTreeArray::Node)   << " bytes\n";
+  cout << "KdTreeCompact::Node\t\t" << sizeof(KdTreeCompact::Node) << " bytes\n";
+
+  cout << "std::size_t\t\t\t"                << sizeof(std::size_t)                    << " bytes\n";
+  cout << "std::vector<KdTreeArray::Node>\t" << sizeof(std::vector<KdTreeArray::Node>) << " bytes\n";
   return 0;
 }
