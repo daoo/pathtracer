df50e732cecddc5c3de6abb288067a88d9a0d422
daoo <daoo314@gmail.com>
2012-12-11 18:08:17 +0100

    Clean up code

--- src/pathtracer/kdtree/optimize.cpp
+++ src/pathtracer/kdtree/optimize.cpp
@@ -16,6 +16,7 @@ namespace kdtree {
 
     void copy(vector<Triangle>& to, const vector<const Triangle*>& from) {
       for (const Triangle* tri : from) {
+        assert(tri != nullptr);
         to.push_back(*tri);
       }
     }
@@ -32,40 +33,16 @@ namespace kdtree {
         } else {
           set(result.m_nodes, index, KdTreeArray::Node());
         }
-      } else if (node->isSplit()) {
+      } else {
+        assert(node->isSplit());
+
         KdTreeLinked::Node* left  = node->m_split.m_left;
         KdTreeLinked::Node* right = node->m_split.m_right;
 
         assert(left != nullptr);
         assert(right != nullptr);
 
-        bool equalChildren = true;
-        if (left->isLeaf() && right->isLeaf() &&
-            left->hasTriangles() && right->hasTriangles() &&
-            left->triangles()->size() == right->triangles()->size()) {
-          auto itl = left->triangles()->cbegin();
-          auto itr = right->triangles()->cbegin();
-
-          while (itl < left->triangles()->cend()) {
-            if (*itl != *itr) {
-              equalChildren = false;
-              break;
-            }
-            ++itl;
-            ++itr;
-          }
-        } else {
-          equalChildren = false;
-        }
-
-        if (equalChildren) {
-          uint32_t i = static_cast<uint32_t>(result.m_leaf_store.size());
-          result.m_leaf_store.push_back(vector<Triangle>());
-          copy(result.m_leaf_store[i], *node->triangles());
-          set(result.m_nodes, index, KdTreeArray::Node(i));
-        } else {
-          set(result.m_nodes, index, KdTreeArray::Node(node->m_split.m_distance));
-        }
+        set(result.m_nodes, index, KdTreeArray::Node(node->m_split.m_distance));
 
         helper(result, KdTreeArray::leftChild(index), left);
         helper(result, KdTreeArray::rightChild(index), right);
