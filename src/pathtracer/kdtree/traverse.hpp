f838ff7fabdd80107403c30ad90785defa679fd3
daoo <daoo314@gmail.com>
2012-12-11 10:10:16 +0100

    Clean up

--- src/pathtracer/kdtree/traverse.hpp
+++ src/pathtracer/kdtree/traverse.hpp
@@ -4,6 +4,7 @@
 #include "math/ray.hpp"
 #include "pathtracer/intersection.hpp"
 #include "pathtracer/kdtree/array.hpp"
+#include "pathtracer/triangle.hpp"
 
 namespace kdtree {
   inline bool searchTree(const KdTreeArray& tree, math::Ray& ray, Intersection& isect) {
