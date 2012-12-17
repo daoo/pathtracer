ad43fe80851bf734f92462e0c5324d2afb0a5ab1
daoo <daoo314@gmail.com>
2012-12-17 10:11:31 +0100

    Tweak behaviour

--- tests/printtree.cpp
+++ tests/printtree.cpp
@@ -9,25 +9,26 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    cout << "Usage: print-tree model.obj\n";
+    return 1;
+  }
+
   typedef high_resolution_clock clock;
   typedef clock::duration time;
 
-  if (argc == 2) {
-    string obj_file = argv[1];
+  string obj_file = argv[1];
 
-    time t1 = clock::now().time_since_epoch();
+  time t1 = clock::now().time_since_epoch();
 
-    OBJModel model;
-    model.load(obj_file);
-    Scene scene(model);
+  OBJModel model;
+  model.load(obj_file);
+  Scene scene(model);
 
-    time t2 = clock::now().time_since_epoch();
+  time t2 = clock::now().time_since_epoch();
 
-    cout << "Built in " << duration_cast<duration<double, ratio<1>>>(t2 - t1).count() << " seconds.\n\n";
-    print(cout, scene.kdtree());
-  } else {
-    cout << "Usage: print-tree model.obj\n";
-  }
+  cout << "Built in " << duration_cast<duration<double, ratio<1>>>(t2 - t1).count() << " seconds.\n\n";
+  print(cout, scene.kdtree());
 
   return 0;
 }
