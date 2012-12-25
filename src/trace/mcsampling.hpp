0789e4b53c70aa68867e846416c192a058f15f67
daoo <daoo314@gmail.com>
2012-12-25 12:16:23 +0100

    Clean up

--- src/trace/mcsampling.hpp
+++ src/trace/mcsampling.hpp
@@ -47,24 +47,22 @@ namespace trace
     if (sx >= -sy) {
       if (sx > sy) {
         // Handle first region of disk
-        r = sx;
-        if (sy > 0.0) theta = sy/r;
-        else          theta = 8.0f + sy/r;
+        r     = sx;
+        theta = sy > 0.0f ? sy / r : 8.0f + sy / r;
       } else {
         // Handle second region of disk
-        r = sy;
-        theta = 2.0f - sx/r;
+        r     = sy;
+        theta = 2.0f - sx / r;
       }
     } else {
       if (sx <= sy) {
         // Handle third region of disk
-        r = -sx;
-        theta = 4.0f - sy/r;
-      }
-      else {
+        r     = -sx;
+        theta = 4.0f - sy / r;
+      } else {
         // Handle fourth region of disk
-        r = -sy;
-        theta = 6.0f + sx/r;
+        r     = -sy;
+        theta = 6.0f + sx / r;
       }
     }
 
