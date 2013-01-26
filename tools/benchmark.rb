039f0594a51256c6ffee03b2eba67fb74a4ba817
daoo <daoo314@gmail.com>
2013-01-26 10:36:31 +0100

    Adjust CSV output

--- tools/benchmark.rb
+++ tools/benchmark.rb
@@ -15,15 +15,43 @@ def measure(scene, width, height, threads, samples)
   end
 end
 
-SCENES  = ["scenes/cornell.obj", "scenes/cube.obj"]
-SIZES   = [[64, 64], [128, 128], [256, 256], [512, 512], [1024, 1024]]
-THREADS = [1, 2, 3, 4, 5, 6, 7, 8]
+SCENES = ["scenes/cornell.obj", "scenes/cube.obj"]
+SIZES  = [[64, 64], [128, 128], [256, 256], [512, 512], [1024, 1024]]
 
-SCENES.each do |scene|
-  SIZES.each do |size|
-    THREADS.each do |threads|
-      print scene, ",", size[0], "x", size[1], ",", threads, ","
-      puts measure(scene, size[0], size[1], threads, threads * 4)
+case ARGV[0]
+  when "threads"
+    WIDTH  = 512
+    HEIGHT = 512
+
+    print "Threads"
+    SCENES.map { |s| print ",", s }
+    puts
+
+    (1..8).each do |threads|
+      print threads
+      SCENES.each do |scene|
+        print ","
+        print measure(scene, WIDTH, HEIGHT, threads, threads * 4)
+      end
+      puts
     end
-  end
+  when "size"
+    THREADS = 1
+    SAMPLES = 32
+
+    print "Sizes"
+    SCENES.map { |s| print ",", s }
+    puts
+
+    SIZES.each do |size|
+      print size[0], "x", size[1]
+      SCENES.each do |scene|
+        print ","
+        print measure(scene, size[0], size[1], THREADS, SAMPLES)
+      end
+      puts
+    end
+  else
+    puts "Usage: benchmark.rb [threads|size]"
+    exit 1
 end
