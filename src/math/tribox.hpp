50a2d50e386652b970ee350a44bba1fb41c9c9f2
daoo <daoo314@gmail.com>
2013-04-28 09:05:09 +0200

    Clean up tri-box intersection

--- src/math/tribox.hpp
+++ src/math/tribox.hpp
@@ -4,8 +4,7 @@
 /********************************************************/
 /* AABB-triangle overlap test code                      */
 /* by Tomas Akenine-Moller                              */
-/* Function: int triBoxOverlap(float boxcenter[3],      */
-/*          float boxhalfsize[3],float triverts[3][3]); */
+/*                                                      */
 /* History:                                             */
 /*   2001-03-05: released the code in its first version */
 /*   2001-06-18: changed the order of the tests, faster */
@@ -16,190 +15,175 @@
 /********************************************************/
 
 #include <glm/glm.hpp>
-#include <math.h>
-#include <stdio.h>
 
 #include "math/aabb.hpp"
-
-#define X 0
-#define Y 1
-#define Z 2
 
 namespace math
 {
   namespace detail
   {
-
-#define CROSS(dest,v1,v2) \
-    dest[0]=v1[1]*v2[2]-v1[2]*v2[1]; \
-    dest[1]=v1[2]*v2[0]-v1[0]*v2[2]; \
-    dest[2]=v1[0]*v2[1]-v1[1]*v2[0];
-
-#define DOT(v1,v2) (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2])
-
-#define SUB(dest,v1,v2) \
-    dest[0]=v1[0]-v2[0]; \
-    dest[1]=v1[1]-v2[1]; \
-    dest[2]=v1[2]-v2[2];
-
-#define FINDMINMAX(x0,x1,x2,min,max) \
-    min = max = x0;   \
-    if(x1<min) min=x1;\
-    if(x1>max) max=x1;\
-    if(x2<min) min=x2;\
-    if(x2>max) max=x2;
-
-    inline int planeBoxOverlap(float normal[3], float vert[3], glm::vec3 maxbox)    // -NJMP-
+    inline void findminmax(float x0, float x1, float x2, float& min, float& max)
     {
-      int q;
-      float vmin[3],vmax[3],v;
-      for(q=X;q<=Z;q++)
-      {
-        v=vert[q];                  // -NJMP-
-        if(normal[q]>0.0f)
-        {
-          vmin[q]=-maxbox[q] - v;   // -NJMP-
-          vmax[q]= maxbox[q] - v;   // -NJMP-
-        }
-        else
-        {
-          vmin[q]= maxbox[q] - v;   // -NJMP-
-          vmax[q]=-maxbox[q] - v;   // -NJMP-
-        }
+      min = x0;
+      max = x0;
+
+      if (x1 < min) min = x1;
+      if (x1 > max) max = x1;
+      if (x2 < min) min = x2;
+      if (x2 > max) max = x2;
+    }
+
+    inline void test_axis(glm::comp axis, const glm::vec3& normal,
+        const glm::vec3& vert, const glm::vec3& maxbox,
+        glm::vec3& vmin, glm::vec3& vmax)
+    {
+      float v = vert[axis];
+      if (normal[axis] > 0.0f) {
+        vmin[axis] = -maxbox[axis] - v;
+        vmax[axis] =  maxbox[axis] - v;
+      } else {
+        vmin[axis] =  maxbox[axis] - v;
+        vmax[axis] = -maxbox[axis] - v;
       }
-      if(DOT(normal,vmin)>0.0f) return 0;   // -NJMP-
-      if(DOT(normal,vmax)>=0.0f) return 1;  // -NJMP-
+    }
+
+    inline int planeBoxOverlap(const glm::vec3& normal, const glm::vec3& vert,
+        glm::vec3 maxbox)
+    {
+      glm::vec3 vmin, vmax;
+      test_axis(glm::X, normal, vert, maxbox, vmin, vmax);
+      test_axis(glm::Y, normal, vert, maxbox, vmin, vmax);
+      test_axis(glm::Z, normal, vert, maxbox, vmin, vmax);
+      if (glm::dot(normal, vmin) > 0.0f) return 0;
+      if (glm::dot(normal, vmax) >= 0.0f) return 1;
 
       return 0;
     }
 
 
     /*======================== X-tests ========================*/
-#define AXISTEST_X01(a, b, fa, fb)                         \
-    p0 = a*v0[Y] - b*v0[Z];                            \
-    p2 = a*v2[Y] - b*v2[Z];                            \
+#define AXISTEST_X01(a, b, fa, fb)                     \
+    p0 = a*v0.y - b*v0.z;                              \
+    p2 = a*v2.y - b*v2.z;                              \
     if(p0<p2) {min=p0; max=p2;} else {min=p2; max=p0;} \
-    rad = fa * aabb.half[Y] + fb * aabb.half[Z];   \
-    if(min>rad || max<-rad) return 0;
+    rad = fa * aabb.half.y + fb * aabb.half.z;         \
+    if(min > rad || max<-rad) return 0;
 
-#define AXISTEST_X2(a, b, fa, fb)                          \
-    p0 = a*v0[Y] - b*v0[Z];                            \
-    p1 = a*v1[Y] - b*v1[Z];                            \
+#define AXISTEST_X2(a, b, fa, fb)                      \
+    p0 = a*v0.y - b*v0.z;                              \
+    p1 = a*v1.y - b*v1.z;                              \
     if(p0<p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
-    rad = fa * aabb.half[Y] + fb * aabb.half[Z];   \
-    if(min>rad || max<-rad) return 0;
+    rad = fa * aabb.half.y + fb * aabb.half.z;         \
+    if(min > rad || max<-rad) return 0;
 
     /*======================== Y-tests ========================*/
-#define AXISTEST_Y02(a, b, fa, fb)                         \
-    p0 = -a*v0[X] + b*v0[Z];                           \
-    p2 = -a*v2[X] + b*v2[Z];                           \
+#define AXISTEST_Y02(a, b, fa, fb)                     \
+    p0 = -a*v0.x + b*v0.z;                             \
+    p2 = -a*v2.x + b*v2.z;                             \
     if(p0<p2) {min=p0; max=p2;} else {min=p2; max=p0;} \
-    rad = fa * aabb.half[X] + fb * aabb.half[Z];   \
-    if(min>rad || max<-rad) return 0;
+    rad = fa * aabb.half.x + fb * aabb.half.z;         \
+    if(min > rad || max<-rad) return 0;
 
-#define AXISTEST_Y1(a, b, fa, fb)                          \
-    p0 = -a*v0[X] + b*v0[Z];                           \
-    p1 = -a*v1[X] + b*v1[Z];                           \
+#define AXISTEST_Y1(a, b, fa, fb)                      \
+    p0 = -a*v0.x + b*v0.z;                             \
+    p1 = -a*v1.x + b*v1.z;                             \
     if(p0<p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
-    rad = fa * aabb.half[X] + fb * aabb.half[Z];   \
-    if(min>rad || max<-rad) return 0;
+    rad = fa * aabb.half.x + fb * aabb.half.z;         \
+    if(min > rad || max<-rad) return 0;
 
     /*======================== Z-tests ========================*/
 
-#define AXISTEST_Z12(a, b, fa, fb)                         \
-    p1 = a*v1[X] - b*v1[Y];                            \
-    p2 = a*v2[X] - b*v2[Y];                            \
+#define AXISTEST_Z12(a, b, fa, fb)                     \
+    p1 = a*v1.x - b*v1.y;                              \
+    p2 = a*v2.x - b*v2.y;                              \
     if(p2<p1) {min=p2; max=p1;} else {min=p1; max=p2;} \
-    rad = fa * aabb.half[X] + fb * aabb.half[Y];   \
-    if(min>rad || max<-rad) return 0;
+    rad = fa * aabb.half.x + fb * aabb.half.y;         \
+    if(min > rad || max<-rad) return 0;
 
-#define AXISTEST_Z0(a, b, fa, fb)                          \
-    p0 = a*v0[X] - b*v0[Y];                            \
-    p1 = a*v1[X] - b*v1[Y];                            \
-    if(p0<p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
-    rad = fa * aabb.half[X] + fb * aabb.half[Y];   \
-    if(min>rad || max<-rad) return 0;
+#define AXISTEST_Z0(a, b, fa, fb)                        \
+    p0 = a*v0.x - b*v0.y;                                \
+    p1 = a*v1.x - b*v1.y;                                \
+    if(p0 < p1) {min=p0; max=p1;} else {min=p1; max=p0;} \
+    rad = fa * aabb.half.x + fb * aabb.half.y;           \
+    if(min > rad || max < -rad) return 0;
   }
 
-  inline bool triBoxOverlap(const Aabb& aabb, const glm::vec3& triv0, const glm::vec3& triv1, const glm::vec3& triv2)
+  inline bool triBoxOverlap(const Aabb& aabb,
+        const glm::vec3& triv0, const glm::vec3& triv1, const glm::vec3& triv2)
   {
     /*    use separating axis theorem to test overlap between triangle and box */
     /*    need to test for overlap in these directions: */
-    /*    1) the {x,y,z}-directions (actually, since we use the AABB of the triangle */
+    /*    1) the {x, y, z}-directions (actually, since we use the AABB of the triangle */
     /*       we do not even need to test these) */
     /*    2) normal of the triangle */
-    /*    3) crossproduct(edge from tri, {x,y,z}-directin) */
+    /*    3) crossproduct(edge from tri, {x, y, z}-directin) */
     /*       this gives 3x3=9 more tests */
-    float v0[3],v1[3],v2[3];
-    //   float axis[3];
-    float min,max,p0,p1,p2,rad,fex,fey,fez;              // -NJMP- "d" local variable removed
-    float normal[3],e0[3],e1[3],e2[3];
-
-    /* This is the fastest branch on Sun */
     /* move everything so that the boxcenter is in (0,0,0) */
-    SUB(v0,triv0,aabb.center);
-    SUB(v1,triv1,aabb.center);
-    SUB(v2,triv2,aabb.center);
+    glm::vec3 v0, v1, v2;
+    v0 = triv0 - aabb.center;
+    v1 = triv1 - aabb.center;
+    v2 = triv2 - aabb.center;
 
     /* compute triangle edges */
-    SUB(e0,v1,v0);      /* tri edge 0 */
-    SUB(e1,v2,v1);      /* tri edge 1 */
-    SUB(e2,v0,v2);      /* tri edge 2 */
+    glm::vec3 e0, e1, e2;
+    e0 = v1 - v0;
+    e1 = v2 - v1;
+    e2 = v0 - v2;
+
+    float min, max, p0, p1, p2, rad;
 
     /* Bullet 3:  */
-    /*  test the 9 tests first (this was faster) */
-    fex = glm::abs<float>(e0[X]);
-    fey = glm::abs<float>(e0[Y]);
-    fez = glm::abs<float>(e0[Z]);
-    AXISTEST_X01(e0[Z], e0[Y], fez, fey);
-    AXISTEST_Y02(e0[Z], e0[X], fez, fex);
-    AXISTEST_Z12(e0[Y], e0[X], fey, fex);
+    /* test the 9 tests first (this was faster) */
+    float fex, fey, fez;
+    fex = glm::abs<float>(e0.x);
+    fey = glm::abs<float>(e0.y);
+    fez = glm::abs<float>(e0.z);
+    AXISTEST_X01(e0.z, e0.y, fez, fey);
+    AXISTEST_Y02(e0.z, e0.x, fez, fex);
+    AXISTEST_Z12(e0.y, e0.x, fey, fex);
 
-    fex = glm::abs<float>(e1[X]);
-    fey = glm::abs<float>(e1[Y]);
-    fez = glm::abs<float>(e1[Z]);
-    AXISTEST_X01(e1[Z], e1[Y], fez, fey);
-    AXISTEST_Y02(e1[Z], e1[X], fez, fex);
-    AXISTEST_Z0(e1[Y], e1[X], fey, fex);
+    fex = glm::abs<float>(e1.x);
+    fey = glm::abs<float>(e1.y);
+    fez = glm::abs<float>(e1.z);
+    AXISTEST_X01(e1.z, e1.y, fez, fey);
+    AXISTEST_Y02(e1.z, e1.x, fez, fex);
+    AXISTEST_Z0(e1.y, e1.x, fey, fex);
 
-    fex = glm::abs<float>(e2[X]);
-    fey = glm::abs<float>(e2[Y]);
-    fez = glm::abs<float>(e2[Z]);
-    AXISTEST_X2(e2[Z], e2[Y], fez, fey);
-    AXISTEST_Y1(e2[Z], e2[X], fez, fex);
-    AXISTEST_Z12(e2[Y], e2[X], fey, fex);
+    fex = glm::abs<float>(e2.x);
+    fey = glm::abs<float>(e2.y);
+    fez = glm::abs<float>(e2.z);
+    AXISTEST_X2(e2.z, e2.y, fez, fey);
+    AXISTEST_Y1(e2.z, e2.x, fez, fex);
+    AXISTEST_Z12(e2.y, e2.x, fey, fex);
 
     /* Bullet 1: */
-    /*  first test overlap in the {x,y,z}-directions */
+    /*  first test overlap in the {x, y, z}-directions */
     /*  find min, max of the triangle each direction, and test for overlap in */
     /*  that direction -- this is equivalent to testing a minimal AABB around */
     /*  the triangle against the AABB */
 
     /* test in X-direction */
-    FINDMINMAX(v0[X],v1[X],v2[X],min,max);
-    if(min>aabb.half[X] || max<-aabb.half[X]) return false;
+    detail::findminmax(v0.x, v1.x, v2.x, min, max);
+    if (min>aabb.half.x || max<-aabb.half.x)
+      return false;
 
     /* test in Y-direction */
-    FINDMINMAX(v0[Y],v1[Y],v2[Y],min,max);
-    if(min>aabb.half[Y] || max<-aabb.half[Y]) return false;
+    detail::findminmax(v0.y, v1.y, v2.y, min, max);
+    if (min>aabb.half.y || max<-aabb.half.y)
+      return false;
 
     /* test in Z-direction */
-    FINDMINMAX(v0[Z],v1[Z],v2[Z],min,max);
-    if(min>aabb.half[Z] || max<-aabb.half[Z]) return false;
+    detail::findminmax(v0.z, v1.z, v2.z, min, max);
+    if (min>aabb.half.z || max<-aabb.half.z)
+      return false;
 
     /* Bullet 2: */
     /*  test if the box intersects the plane of the triangle */
     /*  compute plane equation of triangle: normal*x+d=0 */
-    CROSS(normal,e0,e1);
-    // -NJMP- (line removed here)
-    if(!detail::planeBoxOverlap(normal,v0,aabb.half)) return false;        // -NJMP-
+    glm::vec3 normal = glm::cross(e0, e1);
 
-    return true;   /* box and triangle overlaps */
+    return detail::planeBoxOverlap(normal, v0, aabb.half);
   }
 }
-
-#undef X
-#undef Y
-#undef Z
 
 #endif /* end of include guard: TRIBOX_HPP_R6COW5OF */
