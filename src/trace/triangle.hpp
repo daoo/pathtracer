d4b68b5250076a2325b58cc590578eba997aeacd
Daniel Oom <daoo314@gmail.com>
2015-03-02 18:06:29 +0100

    Clean up variable naming and usage

--- src/trace/triangle.hpp
+++ src/trace/triangle.hpp
@@ -19,41 +19,38 @@ namespace trace
 
   bool intersects(
       const Triangle& tri,
-      const math::Ray& r,
+      const math::Ray& ray,
       float& t,
       glm::vec3& n);
 
   inline bool intersects(
       const Triangle& tri,
-      math::Ray& r,
+      math::Ray& ray,
       float& t,
       glm::vec3& n)
   {
     constexpr float epsilon = 0.00001f;
 
-    const glm::vec3& d = r.direction;
-    const glm::vec3& o = r.origin;
-
     glm::vec3 e1 = tri.v1 - tri.v0;
     glm::vec3 e2 = tri.v2 - tri.v0;
-    glm::vec3 q  = glm::cross(d, e2);
+    glm::vec3 q  = glm::cross(ray.direction, e2);
 
     float a = glm::dot(e1, q);
     if (a > -epsilon && a < epsilon)
       return false;
 
-    glm::vec3 s = o - tri.v0;
+    glm::vec3 s = ray.origin - tri.v0;
     float f = 1.0f / a;
     float u = f * glm::dot(s, q);
     if (u < 0.0 || u > 1.0)
       return false;
 
-    glm::vec3 R = glm::cross(s, e1);
-    float v     = f * glm::dot(d, R);
+    glm::vec3 r = glm::cross(s, e1);
+    float v     = f * glm::dot(ray.direction, r);
     if (v < 0.0 || u + v > 1.0)
       return false;
 
-    t = f * glm::dot(e2, R);
+    t = f * glm::dot(e2, r);
     n = glm::normalize((1.0f - (u + v)) * tri.n0 + u * tri.n1 + v * tri.n2);
 
     return true;
