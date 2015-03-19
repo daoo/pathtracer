45c9c1f731cca31f0d808bc6d4731c89c28a88fb
Daniel Oom <daoo314@gmail.com>
2015-03-19 10:49:20 +0100

    Remove documentation

--- src/trace/samplebuffer.hpp
+++ src/trace/samplebuffer.hpp
@@ -29,75 +29,24 @@ namespace trace
         assert(width > 0 && height > 0);
       }
 
-      /**
-       * Get the width of the buffer.
-       * @return the width of the buffer as an unsigned int, greater than zero
-       */
-      unsigned int width() const
-      {
-        return m_width;
-      }
+      unsigned int width()  const { return m_width;  }
+      unsigned int height() const { return m_height; }
 
-      /**
-       * Get the height of the buffer.
-       * @return the height of the buffer as an unsigned int, greater than zero
-       */
-      unsigned int height() const
-      {
-        return m_height;
-      }
+      unsigned int samples() const { return m_samples; }
 
-      /**
-       * Return the number of samples in this buffer.
-       * Can be zero if no samples have been added.
-       * @return an unsigned int greater than or equal to zero
-       */
-      unsigned int samples() const
-      {
-        return m_samples;
-      }
+      void inc() { ++m_samples; }
 
-      /**
-       * Increase the number of samples by one.
-       */
-      void inc()
-      {
-        ++m_samples;
-      }
-
-      /**
-       * Retrive the RGB color stored at a certain location in the buffer.
-       * @param x the x location, must be less than the width
-       * @param y the y location, must be less than the height
-       * @return the color at the position or black (#000000) if the location
-       * havn't been added to yet.
-       */
       const glm::vec3& get(unsigned int x, unsigned int y) const
       {
         return m_buffer[y * m_width + x];
       }
 
-      /**
-       * Add color sample to certain location in the buffer.
-       * @param x the x location, must be less than the width
-       * @param y the y location, must be less than the height
-       */
       void add(unsigned int x, unsigned int y, const glm::vec3& v)
       {
         m_buffer[y * m_width + x] += v;
       }
 
-      /**
-       * Return a pointer to the underlying storage array.
-       * For example this could be used to feed the buffer data to a OpenGL
-       * texture.
-       * @return a constant pointer to the underlying array, guaranteed to be
-       * non-null
-       */
-      const glm::vec3* data() const
-      {
-        return m_buffer.data();
-      }
+      const glm::vec3* data() const { return m_buffer.data(); }
 
       /**
        * Append another buffer to this one.
