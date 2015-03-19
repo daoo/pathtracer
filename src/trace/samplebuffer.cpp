4909e314d2f4c5d671165e3bb3c758f54e458829
Daniel Oom <daoo314@gmail.com>
2015-03-19 10:45:08 +0100

    Clean up image writing code

--- src/trace/samplebuffer.cpp
+++ src/trace/samplebuffer.cpp
@@ -15,42 +15,34 @@ namespace trace
     {
       return glm::min(1.0f, pow(x, GAMMA_POWER));
     }
-
-    vec3 gamma_correct(const vec3& v)
-    {
-      return vec3(gamma_correct(v.r), gamma_correct(v.g), gamma_correct(v.b));
-    }
   }
 
   void write_image(const string& file, const SampleBuffer& buffer)
   {
     const float samples = static_cast<float>(buffer.samples());
-    FIBITMAP* dib = FreeImage_Allocate(buffer.width(), buffer.height(),
-        32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK);
+    FIBITMAP* bitmap = FreeImage_Allocate(
+        buffer.width(),
+        buffer.height(),
+        24);
 
-    const unsigned int BYTESPP =
-      FreeImage_GetLine(dib) / FreeImage_GetWidth(dib);
-    for (unsigned int y = 0; y < FreeImage_GetHeight(dib); ++y) {
-      BYTE* bits = FreeImage_GetScanLine(dib, y);
-
-      for (unsigned int x = 0; x < FreeImage_GetWidth(dib); ++x) {
-        vec3 c = gamma_correct(buffer.get(x, y) / samples) * 255.0f;
-        bits[FI_RGBA_RED]   = static_cast<BYTE>(c.r);
-        bits[FI_RGBA_GREEN] = static_cast<BYTE>(c.g);
-        bits[FI_RGBA_BLUE]  = static_cast<BYTE>(c.b);
-        bits[FI_RGBA_ALPHA] = 255;
-
-        bits += BYTESPP;
+    for (unsigned int y = 0; y < FreeImage_GetHeight(bitmap); ++y) {
+      for (unsigned int x = 0; x < FreeImage_GetWidth(bitmap); ++x) {
+        vec3 pixel = buffer.get(x, y);
+        RGBQUAD rgb;
+        rgb.rgbRed   = static_cast<BYTE>(gamma_correct(pixel.r / samples) * 255.0f);
+        rgb.rgbGreen = static_cast<BYTE>(gamma_correct(pixel.g / samples) * 255.0f);
+        rgb.rgbBlue  = static_cast<BYTE>(gamma_correct(pixel.b / samples) * 255.0f);
+        FreeImage_SetPixelColor(bitmap, x, y, &rgb);
       }
     }
 
-    if (!FreeImage_Save(FIF_PNG, dib, file.c_str(), 0)) {
+    if (!FreeImage_Save(FIF_PNG, bitmap, file.c_str(), 0)) {
       string err = "Failed to save screenshot to file '";
       err += file;
       err += '\'';
       throw err;
     }
 
-    FreeImage_Unload(dib);
+    FreeImage_Unload(bitmap);
   }
 }
