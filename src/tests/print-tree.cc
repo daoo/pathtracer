e4e12f471c662c390d36e488c103ceded992d3dd
Daniel Oom <daoo314@gmail.com>
2017-02-05 16:28:35 +0100

    Don't convert kdtree for printing

--- src/tests/print-tree.cc
+++ src/tests/print-tree.cc
@@ -1,7 +1,8 @@
 #include <experimental/filesystem>
 #include <iostream>
 
-#include "kdtree/array.h"
+#include "kdtree/linked.h"
+#include "kdtree/surface_area_heuristic.h"
 #include "kdtree/util.h"
 #include "trace/scene.h"
 #include "util/clock.h"
@@ -16,32 +17,21 @@ std::ostream& operator<<(std::ostream& stream, kdtree::Axis axis) {
 }
 
 void helper(const std::string& label,
-            const kdtree::KdTreeArray& tree,
-            unsigned int index,
-            kdtree::Axis axis,
+            const kdtree::KdNodeLinked* node,
             unsigned int depth) {
-  for (unsigned int i = 0; i < depth; ++i) {
-    std::cout << "  ";
-  }
-
-  kdtree::KdNodeArray node = tree.get_node(index);
-
-  if (node.is_leaf()) {
-    std::cout << "Leaf: " << tree.get_triangles(node).size() << "\n";
-  } else if (node.is_split()) {
-    std::cout << "Split: " << label << ", " << axis << ", " << node.get_split()
-              << "\n";
-    helper("left", tree, kdtree::KdTreeArray::left_child(index),
-           next_axis(axis), depth + 1);
-    helper("right", tree, kdtree::KdTreeArray::right_child(index),
-           next_axis(axis), depth + 1);
+  std::cout << std::string(depth * 2, ' ');
+  if (node->triangles != nullptr) {
+    std::cout << "Leaf: " << node->triangles->size() << "\n";
   } else {
-    assert(false && "Node not leaf or split");
+    std::cout << "Split: " << label << ", " << node->axis << ", "
+              << node->distance << "\n";
+    helper("left", node->left, depth + 1);
+    helper("right", node->right, depth + 1);
   }
 }
 
-void print(const kdtree::KdTreeArray& tree) {
-  helper("root", tree, 0, kdtree::X, 0);
+void print(const kdtree::KdNodeLinked* tree) {
+  helper("root", tree, 0);
 }
 
 int main(int argc, char* argv[]) {
@@ -55,10 +45,9 @@ int main(int argc, char* argv[]) {
       trace::triangles_from_obj(wavefront::load_obj(obj_file));
 
   util::Clock clock;
-  kdtree::KdTreeArray kdtree = trace::kdtree_from_triangles(triangles);
+  kdtree::KdNodeLinked* kdtree = kdtree::build_tree_sah(triangles);
   float construction_time = clock.measure<float, std::ratio<1>>();
-
-  std::cout << "Built in " << construction_time << " seconds.\n\n";
+  std::cerr << "Built in " << construction_time << "s.\n";
   print(kdtree);
 
   return 0;
