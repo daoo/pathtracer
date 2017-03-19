2258e8762f78c5f8560bf396c13d70cc1cceb284
Daniel Oom <daoo314@gmail.com>
2017-03-19 11:11:22 +0100

    Clarify half size naming

--- src/kdtree/util.cc
+++ src/kdtree/util.cc
@@ -24,17 +24,17 @@ AabbResults split_aabb(const Aabb& parent, const Aap& plane) {
   float min = parent.GetMin()[plane.GetAxis()];
   float max = parent.GetMax()[plane.GetAxis()];
   float split = plane.GetDistance();
-  float lh = (split - min) / 2.0f + glm::epsilon<float>();
-  float rh = (max - split) / 2.0f + glm::epsilon<float>();
-  assert(lh > 0 && rh > 0);
+  float left_half_axis = (split - min) / 2.0f + glm::epsilon<float>();
+  float right_half_axis = (max - split) / 2.0f + glm::epsilon<float>();
+  assert(left_half_axis > 0 && right_half_axis > 0);
 
   glm::vec3 left_center(parent.GetCenter()), left_half(parent.GetHalf());
-  left_center[plane.GetAxis()] = split - lh;
-  left_half[plane.GetAxis()] = lh;
+  left_center[plane.GetAxis()] = split - left_half_axis;
+  left_half[plane.GetAxis()] = left_half_axis;
 
   glm::vec3 right_center(parent.GetCenter()), right_half(parent.GetHalf());
-  right_center[plane.GetAxis()] = split + rh;
-  right_half[plane.GetAxis()] = rh;
+  right_center[plane.GetAxis()] = split + right_half_axis;
+  right_half[plane.GetAxis()] = right_half_axis;
 
   return {Aabb(left_center, left_half), Aabb(right_center, right_half)};
 }
