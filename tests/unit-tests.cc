a57b07c2667eabeb763c4119160e5aa7943a2303
Daniel Oom <daoo314@gmail.com>
2017-05-05 21:44:17 +0200

    Add a basic unit test to build system

--- tests/unit-tests.cc
+++ tests/unit-tests.cc
@@ -1,2 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include "tests/catch.h"
+
+#include <glm/glm.hpp>
+
+#include "geometry/triangle.h"
+
+TEST_CASE("Triangle min/max", "[triangle]") {
+  geometry::Triangle tri;
+  tri.v0 = {1, 1, 1};
+  tri.v1 = {2, 2, 2};
+  tri.v2 = {3, 3, 3};
+  REQUIRE(tri.GetMin() == glm::vec3(1, 1, 1));
+  REQUIRE(tri.GetMax() == glm::vec3(3, 3, 3));
+}
