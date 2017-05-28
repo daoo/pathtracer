845b7747b618fe4129321360f5caa4d8c230bdf0
Daniel Oom <daoo314@gmail.com>
2017-05-28 14:16:51 +0200

    Move unit tests to separate files

---
+++ geometry/triangle_unittest.cc
@@ -0,0 +1,13 @@
+#include <glm/glm.hpp>
+
+#include "geometry/triangle.h"
+#include "tests/catch.h"
+
+TEST_CASE("Triangle min/max", "[triangle]") {
+  geometry::Triangle tri;
+  tri.v0 = {1, 1, 1};
+  tri.v1 = {2, 2, 2};
+  tri.v2 = {3, 3, 3};
+  REQUIRE(tri.GetMin() == glm::vec3(1, 1, 1));
+  REQUIRE(tri.GetMax() == glm::vec3(3, 3, 3));
+}
