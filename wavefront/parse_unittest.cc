237597a0fab9b3df2127198df9ccad8b20eb11ba
Daniel Oom <daoo314@gmail.com>
2017-06-04 15:44:52 +0200

    Give tests unique names

--- wavefront/parse_unittest.cc
+++ wavefront/parse_unittest.cc
@@ -1,7 +1,7 @@
 #include "tests/catch.h"
 #include "wavefront/parse.h"
 
-TEST_CASE("zero", "[parse_int]") {
+TEST_CASE("parse integer zero", "[wavefront]") {
   const char* str = "0";
   const char* end = nullptr;
   int value = wavefront::parse_int(str, &end);
@@ -9,7 +9,7 @@ TEST_CASE("zero", "[parse_int]") {
   REQUIRE(end == str + 1);
 }
 
-TEST_CASE("positive int", "[parse_int]") {
+TEST_CASE("parse positive integer", "[wavefront]") {
   const char* str = "1";
   const char* end = nullptr;
   int value = wavefront::parse_int(str, &end);
@@ -17,7 +17,7 @@ TEST_CASE("positive int", "[parse_int]") {
   REQUIRE(end == str + 1);
 }
 
-TEST_CASE("negative int", "[parse_int]") {
+TEST_CASE("parse negative integer", "[wavefront]") {
   const char* str = "-1";
   const char* end = nullptr;
   int value = wavefront::parse_int(str, &end);
@@ -25,7 +25,7 @@ TEST_CASE("negative int", "[parse_int]") {
   REQUIRE(end == str + 2);
 }
 
-TEST_CASE("largish int", "[parse_int]") {
+TEST_CASE("parse largish integer", "[wavefront]") {
   const char* str = "12345";
   const char* end = nullptr;
   int value = wavefront::parse_int(str, &end);
@@ -33,47 +33,47 @@ TEST_CASE("largish int", "[parse_int]") {
   REQUIRE(end == str + 5);
 }
 
-TEST_CASE("empty", "[equal]") {
+TEST_CASE("equal empty strings", "[wavefront]") {
   REQUIRE(wavefront::equal("", ""));
 }
 
-TEST_CASE("example", "[equal]") {
+TEST_CASE("equal example strings", "[wavefront]") {
   REQUIRE(wavefront::equal("a", "a"));
 }
 
-TEST_CASE("not equal example", "[equal]") {
+TEST_CASE("equal different strings", "[wavefront]") {
   REQUIRE(!wavefront::equal("a", "b"));
 }
 
-TEST_CASE("first longer", "[equal]") {
+TEST_CASE("equal first string longer", "[wavefront]") {
   REQUIRE(!wavefront::equal("a", ""));
 }
 
-TEST_CASE("second longer", "[equal]") {
+TEST_CASE("equal second string longer", "[wavefront]") {
   REQUIRE(!wavefront::equal("", "a"));
 }
 
-TEST_CASE("empty", "[skip_whitespace]") {
+TEST_CASE("skip whitespace empty", "[wavefront]") {
   const char* str = "";
   REQUIRE(wavefront::skip_whitespace(str) == str);
 }
 
-TEST_CASE("skip one space", "[skip_whitespace]") {
+TEST_CASE("skip whitespace one space", "[wavefront]") {
   const char* str = " ";
   REQUIRE(wavefront::skip_whitespace(str) == str + 1);
 }
 
-TEST_CASE("skip one tab", "[skip_whitespace]") {
+TEST_CASE("skip whitespace one tab", "[wavefront]") {
   const char* str = "\t";
   REQUIRE(wavefront::skip_whitespace(str) == str + 1);
 }
 
-TEST_CASE("skip many whites", "[skip_whitespace]") {
+TEST_CASE("skip whitespace several", "[wavefront]") {
   const char* str = " \t \t ";
   REQUIRE(wavefront::skip_whitespace(str) == str + 5);
 }
 
-TEST_CASE("do not skip non-whitespace", "[skip_whitespace]") {
+TEST_CASE("skip whitespace non-whitespace", "[wavefront]") {
   const char* str = "apa";
   REQUIRE(wavefront::skip_whitespace(str) == str);
 }
