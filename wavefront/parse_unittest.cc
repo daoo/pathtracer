a965d02d6f6b4a3e067bf95440fd8f4cba309aa1
Daniel Oom <daoo314@gmail.com>
2017-06-20 10:57:58 +0200

    Add more parser tests

--- wavefront/parse_unittest.cc
+++ wavefront/parse_unittest.cc
@@ -79,3 +79,69 @@ TEST_CASE("skip whitespace non-whitespace", "[wavefront]") {
   parse.SkipWhitespace();
   REQUIRE(parse.ParseString() == "apa");
 }
+
+TEST_CASE("skip zero", "[wavefront]") {
+  wavefront::Parse parse("apa");
+  parse.Skip(0);
+  REQUIRE(parse.ParseString() == "apa");
+}
+
+TEST_CASE("skip one", "[wavefront]") {
+  wavefront::Parse parse("apa");
+  parse.Skip(1);
+  REQUIRE(parse.ParseString() == "pa");
+}
+
+TEST_CASE("skip three", "[wavefront]") {
+  wavefront::Parse parse("apa");
+  parse.Skip(3);
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse float zero", "[wavefront]") {
+  wavefront::Parse parse("0");
+  REQUIRE(parse.ParseFloat() == 0.0f);
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse float zero decimal", "[wavefront]") {
+  wavefront::Parse parse("0.0");
+  REQUIRE(parse.ParseFloat() == 0.0f);
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse float 1.5", "[wavefront]") {
+  wavefront::Parse parse("1.5");
+  REQUIRE(parse.ParseFloat() == 1.5f);
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse float -1.5", "[wavefront]") {
+  wavefront::Parse parse("-1.5");
+  REQUIRE(parse.ParseFloat() == -1.5f);
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse vec2", "[wavefront]") {
+  wavefront::Parse parse("1.5 1.5");
+  REQUIRE(parse.ParseVec2() == glm::vec2(1.5f, 1.5f));
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse vec2 multiple space", "[wavefront]") {
+  wavefront::Parse parse("1.5  1.5");
+  REQUIRE(parse.ParseVec2() == glm::vec2(1.5f, 1.5f));
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse vec3", "[wavefront]") {
+  wavefront::Parse parse("1.5 2.5 3.5");
+  REQUIRE(parse.ParseVec3() == glm::vec3(1.5f, 2.5f, 3.5f));
+  REQUIRE(parse.AtEnd());
+}
+
+TEST_CASE("parse vec3 multiple space", "[wavefront]") {
+  wavefront::Parse parse("1.5  2.5  3.5");
+  REQUIRE(parse.ParseVec3() == glm::vec3(1.5f, 2.5f, 3.5f));
+  REQUIRE(parse.AtEnd());
+}
