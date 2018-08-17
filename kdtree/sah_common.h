20ec2d9f1bb84444c41d2012268af0e3ddd3e575
Daniel Oom <daoo314@gmail.com>
2018-08-17 19:50:18 +0200

    Tweak SAH parameters

--- kdtree/sah_common.h
+++ kdtree/sah_common.h
@@ -3,9 +3,9 @@
 
 namespace kdtree {
 
-constexpr float COST_TRAVERSE = 0.01f;
+constexpr float COST_EMPTY = 0.01f;
+constexpr float COST_TRAVERSE = 0.1f;
 constexpr float COST_INTERSECT = 1.0f;
-constexpr float COST_EMPTY = 0.8f;
 
 float LeafCostBound(size_t parent_count) {
   return COST_INTERSECT * parent_count;
@@ -16,10 +16,11 @@ float CalculateCost(float parent_area,
                     float right_area,
                     size_t left_count,
                     size_t right_count) {
-  float factor = left_count == 0 || right_count == 0 ? COST_EMPTY : 1.0f;
-  float intersect =
-      (left_area * left_count + right_area * right_count) / parent_area;
-  return factor * COST_TRAVERSE + COST_INTERSECT * intersect;
+  float traverse =
+      left_count == 0 || right_count == 0 ? COST_EMPTY : COST_TRAVERSE;
+  float area_heuristic = (left_area * left_count + right_area * right_count);
+  float intersect = COST_INTERSECT * area_heuristic / parent_area;
+  return traverse + intersect;
 }
 
 }  // namespace kdtree
