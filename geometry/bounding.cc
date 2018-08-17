f4f1a7ccb3056604055383ad5450abc05b331d4e
Daniel Oom <daoo314@gmail.com>
2018-08-17 17:38:07 +0200

    Fix uninitialized values in find_bounding

--- geometry/bounding.cc
+++ geometry/bounding.cc
@@ -1,6 +1,7 @@
 #include "geometry/bounding.h"
 
 #include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
 
 #include "geometry/aabb.h"
 #include "geometry/triangle.h"
@@ -10,19 +11,20 @@ using std::vector;
 
 namespace geometry {
 Aabb find_bounding(const vector<Triangle>& triangles) {
-  vec3 min, max;
-
-  for (const Triangle& tri : triangles) {
-    min = glm::min(min, tri.v0);
-    min = glm::min(min, tri.v1);
-    min = glm::min(min, tri.v2);
-
-    max = glm::max(max, tri.v0);
-    max = glm::max(max, tri.v1);
-    max = glm::max(max, tri.v2);
+  if (triangles.size() == 0) {
+    return {glm::zero<vec3>(), glm::zero<vec3>()};
   }
 
-  vec3 half = (max - min) / 2.0f;
-  return {min + half, half};
+  vec3 a = triangles[0].GetMin();
+  vec3 b = triangles[0].GetMax();
+
+  for (size_t i = 1; i < triangles.size(); ++i) {
+    const Triangle& tri = triangles[i];
+    a = glm::min(a, tri.GetMin());
+    b = glm::max(b, tri.GetMax());
+  }
+
+  vec3 half = (b - a) / 2.0f;
+  return {a + half, half};
 }
 }  // namespace geometry
