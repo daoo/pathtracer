2ddce73ac93b95f579b1288c273203dd6c79ce95
Daniel Oom <daoo314@gmail.com>
2018-08-17 19:41:15 +0200

    Default to non-fast sah implementation

--- tools/kdtree-png.sh
+++ tools/kdtree-png.sh
@@ -5,6 +5,6 @@ set -eu
 dir="out_clang_debug"
 inp="$1"
 
-./$dir/print-tree-svg-sah_fast "$inp" > /tmp/test.svg
+./$dir/print-tree-svg-sah "$inp" > /tmp/test.svg
 inkscape --without-gui --export-png /tmp/test.png -D /tmp/test.svg
 rm /tmp/test.svg
