0fde31f1255bca6415ba6d5b6e0cebc0237b372f
Daniel Oom <daoo314@gmail.com>
2018-08-19 17:05:02 +0200

    Run lint.sh in continuous-build.sh

--- continuous-build.sh
+++ continuous-build.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-rg --files --type cpp | entr -c sh -c "ninja -C out_clang_debug && ./out_clang_debug/unit-tests"
+rg --files --type cpp | entr -c sh -c "ninja -C out_clang_debug && ./lint.sh && ./out_clang_debug/unit-tests"
