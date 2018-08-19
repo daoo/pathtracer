8228410a80ee20ea6151773318e1e7d461b45b49
Daniel Oom <daoo314@gmail.com>
2018-08-19 16:51:53 +0200

    Run cppcheck in lint script

--- lint.sh
+++ lint.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
-format-check() {
-  diff -u <(cat $@) <(clang-format $@)
+list-source() {
+  find \
+    geometry kdtree pathtracer pathtracer-gl tests trace util wavefront \
+    -type f -and -not -name catch.h
 }
 
-files="$(find geometry kdtree pathtracer pathtracer-gl tests trace util wavefront -type f -and -not -name catch.h)"
+format-check() {
+  diff -u <(cat "$@") <(clang-format "$@")
+}
 
-cpplint --quiet --filter=-legal/copyright,-build/c++11,-readability/todo --quiet $files 2>&1 >/dev/null | grep -v '#include <algorithm>'
+call-cpplint() {
+  cpplint \
+    --quiet \
+    --filter=-legal/copyright,-build/c++11,-readability/todo \
+    "$@"
+}
 
+call-cppcheck() {
+  cppcheck \
+    --language=c++ \
+    --std=c++11 \
+    --enable=all \
+    --quiet \
+    "$@"
+}
+
+files="$(list-source)"
+{ call-cpplint $files > /dev/null; } 2>&1 | grep -v '#include <algorithm>'
+call-cppcheck $files
 format-check $files
