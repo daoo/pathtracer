7b5a84d71f8a868ad3a10f0ebb60fd2139c3d7c8
Daniel Oom <daoo314@gmail.com>
2024-03-01 15:20:11 +0100

    Refactor out src/bin/wavefront-print.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-mod wavefront;
-
-use std::env;
-use std::fs;
-use std::str;
-use std::path::Path;
-use crate::wavefront::{mtl, obj};
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = Path::new(&args[1]);
-    let bytes = fs::read(path).unwrap();
-    let input = str::from_utf8(&bytes).unwrap();
-    match path.extension().and_then(|s| s.to_str()) {
-        Some("obj") => println!("{:?}", obj::obj(&input)),
-        Some("mtl") => println!("{:?}", mtl::mtl(&input)),
-        _ => panic!("Unexpected file extension for {:?}", path),
-    }
+    println!("Hello world!");
 }
