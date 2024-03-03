58c98eefe0554c4fd8addb33926ef835cd2952fa
Daniel Oom <daoo314@gmail.com>
2024-03-03 19:14:00 +0100

    Implement wavefront to scene conversion

--- src/light.rs
+++ src/light.rs
@@ -8,11 +8,15 @@ pub struct SphericalLight {
 }
 
 impl SphericalLight {
-    pub fn from_color(center: Vector3<f32>, radius: f32, color: &Vector3<f32>, intensity: f32) -> SphericalLight {
+    pub fn new(center: Vector3<f32>, radius: f32, color: &Vector3<f32>, intensity: f32) -> SphericalLight {
         SphericalLight {
             center,
             intensity: color * intensity,
             radius
         }
+    }
+
+    pub fn emitted(&self, point: Vector3<f32>) -> Vector3<f32> {
+        self.intensity / (self.center - point).norm_squared()
     }
 }
--- src/scene.rs
+++ src/scene.rs
@@ -4,6 +4,7 @@ use crate::geometry::ray::*;
 use crate::geometry::triangle::*;
 use crate::light::*;
 use crate::material::*;
+use crate::wavefront::*;
 use std::rc::Rc;
 
 pub struct Scene {
@@ -11,6 +12,81 @@ pub struct Scene {
     pub materials: Vec<Rc<dyn Material>>,
     pub cameras: Vec<Camera>,
     pub lights: Vec<SphericalLight>,
+}
+
+fn triangles_from_obj(obj: &obj::Obj) -> Vec<Triangle> {
+    obj.chunks
+        .iter()
+        .map(|chunk| chunk.faces
+             .iter()
+             .map(|face| Triangle {
+                v0: obj.index_vertex(&face.p0),
+                v1: obj.index_vertex(&face.p1),
+                v2: obj.index_vertex(&face.p2),
+             }))
+        .flatten()
+        .collect()
+}
+
+fn blend1_from_mtl(material: &mtl::Material) -> Rc<dyn Material> {
+    let refraction = SpecularRefractiveMaterial { index_of_refraction: material.index_of_refraction };
+    let reflection = DiffuseReflectiveMaterial { reflectance: material.diffuse_reflection };
+    if material.transparency >= 0.99 {
+        Rc::new(refraction)
+    } else if material.transparency <= 0.01 {
+        Rc::new(reflection)
+    } else {
+        Rc::new(BlendMaterial {
+            first: Rc::new(refraction),
+            second: Rc::new(reflection),
+            factor: material.transparency
+        })
+    }
+}
+
+fn blend0_from_mtl(material: &mtl::Material, blend1: Rc<dyn Material>) -> Rc<dyn Material> {
+    let specular = SpecularReflectiveMaterial { reflectance: material.specular_reflection };
+    if material.reflection_90_degrees >= 0.99 {
+        Rc::new(FresnelBlendMaterial {
+            reflection: Rc::new(specular),
+            refraction: blend1,
+            r0: material.reflection_0_degrees,
+        })
+    } else if material.reflection_90_degrees <= 0.01 {
+        blend1
+    } else {
+        Rc::new(BlendMaterial {
+            first: Rc::new(FresnelBlendMaterial {
+                reflection: Rc::new(specular),
+                refraction: blend1.clone(),
+                r0: material.reflection_0_degrees,
+            }),
+            second: blend1,
+            factor: material.reflection_90_degrees
+        })
+    }
+}
+
+fn material_from_mtl(material: &mtl::Material) -> Rc<dyn Material> {
+    blend0_from_mtl(&material, blend1_from_mtl(&material))
+}
+
+fn materials_from_mtl(mtl: &mtl::Mtl) -> Vec<Rc<dyn Material>> {
+    mtl.materials.iter().map(material_from_mtl).collect()
+}
+
+fn cameras_from_mtl(mtl: &mtl::Mtl) -> Vec<Camera> {
+    mtl.cameras
+        .iter()
+        .map(|camera| Camera::new(&camera.position, &camera.target, &camera.up, camera.fov))
+        .collect()
+}
+
+fn lights_from_mtl(mtl: &mtl::Mtl) -> Vec<SphericalLight> {
+    mtl.lights
+        .iter()
+        .map(|light| SphericalLight::new(light.position, light.radius, &light.color, light.intensity))
+        .collect()
 }
 
 impl Scene {
@@ -21,5 +97,14 @@ impl Scene {
 
     pub fn intersect_any(&self, ray: &Ray, tmin: f32, tmax: f32) -> bool {
         self.intersect(ray, tmin, tmax).is_some()
+    }
+
+    pub fn from_wavefront(obj: &obj::Obj, mtl: &mtl::Mtl) -> Scene {
+        Scene {
+            triangles: triangles_from_obj(obj),
+            materials: materials_from_mtl(mtl),
+            cameras: cameras_from_mtl(mtl),
+            lights: lights_from_mtl(mtl),
+        }
     }
 }
