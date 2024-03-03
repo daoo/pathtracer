2ff61bd1037ea2bfa81080dbf8e018c1077bc6c3
Daniel Oom <daoo314@gmail.com>
2024-03-03 20:52:16 +0100

    Fix pinhole camera ray calculation

--- src/camera.rs
+++ src/camera.rs
@@ -7,18 +7,18 @@ pub struct Camera {
     pub direction: UnitVector3<f32>,
     pub up: UnitVector3<f32>,
     pub right: UnitVector3<f32>,
-    pub fov: f32,
+    pub fov_degrees: f32,
 }
 
 impl Camera {
-    pub fn new(position: &Vector3<f32>, target: &Vector3<f32>, up: &Vector3<f32>, fov: f32) -> Camera {
+    pub fn new(position: &Vector3<f32>, target: &Vector3<f32>, up: &Vector3<f32>, fov_degrees: f32) -> Camera {
         let direction = UnitVector3::new_normalize(target - position);
         Camera {
             position: position.clone(),
             direction,
             up: UnitVector3::new_normalize(*up),
             right: UnitVector3::new_normalize(direction.cross(up)),
-            fov
+            fov_degrees
         }
     }
 }
@@ -26,30 +26,53 @@ impl Camera {
 #[derive(Clone, Debug)]
 pub struct Pinhole {
     pub position: Vector3<f32>,
-    pub origin: Vector3<f32>,
+    pub plane: Vector3<f32>,
     pub dx: Vector3<f32>,
     pub dy: Vector3<f32>,
 }
 
 impl Pinhole {
     pub fn new(camera: &Camera, aspect_ratio: f32) -> Pinhole {
-        let fov_half = camera.fov / 2.0;
-        let x = camera.up.into_inner() * fov_half.sin();
-        let y = camera.right.into_inner() * fov_half.sin() * aspect_ratio;
-        let z = camera.direction.into_inner() * fov_half.cos();
+        let half_fov_radians = camera.fov_degrees * std::f32::consts::PI / 360.0;
+        let x = camera.right.into_inner() * (half_fov_radians.sin() * aspect_ratio);
+        let y = camera.up.into_inner() * half_fov_radians.sin();
+        let z = camera.direction.into_inner() * half_fov_radians.cos();
 
         Pinhole {
             position: camera.position,
-            origin: z - y - x,
-            dx: 2.0 * y,
-            dy: 2.0 * x,
+            plane: z + y - x,
+            dx: 2.0 * x,
+            dy: -2.0 * y,
         }
     }
 
     pub fn ray(&self, x: f32, y: f32) -> Ray {
         Ray {
             origin: self.position,
-            direction: self.origin + x * self.dx + y * self.dy,
+            direction: self.plane + x * self.dx + y * self.dy,
         }
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use nalgebra::vector;
+
+//     #[test]
+//     fn ray_in_image_plane_center() {
+//         let camera = Camera::new(
+//             &vector![1.0, 0.0, 0.0],
+//             &vector![-1.0, 0.0, 0.0],
+//             &vector![0.0, 1.0, 0.0],
+//             1.0,
+//         );
+//         let pinhole = Pinhole::new(&camera, 1.0);
+
+//         let actual = pinhole.ray(0.5, 0.5);
+
+//         let direction_error = (actual.direction - vector![-1.0, 0.0, 0.0]).norm();
+//         assert_eq!(actual.origin, vector![1.0, 0.0, 0.0]);
+//         assert!(direction_error <= 0.1, "direction_error = {}", direction_error);
+//     }
+// }
