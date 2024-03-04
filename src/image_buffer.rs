a4c837e732a61b6ef87840b7a3c19455968d3aa7
Daniel Oom <daoo314@gmail.com>
2024-03-04 20:31:57 +0100

    Add missing gamma correction

--- src/image_buffer.rs
+++ src/image_buffer.rs
@@ -3,6 +3,18 @@ use std::{path::Path, ops::{Index, IndexMut}};
 
 pub struct ImageBuffer(DMatrix<Vector3<f32>>);
 
+fn gamma_correct(x: f32) -> f32 {
+    const GAMMA_POWER: f32 = 1.0 / 2.2;
+    return 1.0f32.min(x.powf(GAMMA_POWER));
+}
+
+fn to_rgb_u8(vector: &Vector3<f32>) -> image::Rgb<u8> {
+    image::Rgb([
+        (gamma_correct(vector.x) * 255.0) as u8,
+        (gamma_correct(vector.y) * 255.0) as u8,
+        (gamma_correct(vector.z) * 255.0) as u8])
+}
+
 impl ImageBuffer {
     pub fn new(width: usize, height: usize) -> Self {
         ImageBuffer(DMatrix::zeros(width, height))
@@ -19,8 +31,7 @@ impl ImageBuffer {
         let mut image = image::RgbImage::new(self.0.ncols() as u32, self.0.nrows() as u32);
         for y in 0..self.0.nrows() {
             for x in 0..self.0.ncols() {
-                let c = self.0[(x, y)] * 255.0;
-                image[(x as u32, y as u32)] = image::Rgb([c.x as u8, c.y as u8, c.z as u8]);
+                image[(x as u32, y as u32)] = to_rgb_u8(&self.0[(x, y)]);
             }
         }
         image.save_with_format(path, format)
