4a4f30de39844799145606a9e4255b9301c907a0
Daniel Oom <daoo314@gmail.com>
2024-03-13 20:02:49 +0100

    Also group rays by iteration

--- visualize.py
+++ visualize.py
@@ -10,10 +10,8 @@ rerun.connect()
 
 rerun.log('rays', rerun.ViewCoordinates.RIGHT_HAND_Y_UP, timeless=True)
 
-for (_, ray) in rays.groupby(['px', 'py']):
+for (_, ray) in rays.groupby(['i', 'px', 'py']):
     segments = ray[['ax', 'ay', 'az', 'bx', 'by', 'bz']].to_numpy()
     segments = segments.reshape((len(ray), 2, 3))
-    rerun.log(
-            f'rays/{int(ray.i.iloc[0])}@{int(ray.px.iloc[0])}x{int(ray.py.iloc[0])}',
-            rerun.LineStrips3D(segments, radii=0.001),
-            timeless=True)
+    path = f'rays/iter{int(ray.i.iloc[0])}/{int(ray.px.iloc[0])}x{int(ray.py.iloc[0])}'
+    rerun.log(path, rerun.LineStrips3D(segments, radii=0.001), timeless=True)
