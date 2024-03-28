29035e3a0fd0ba36e41ca250e4740ada84315048
Daniel Oom <daoo314@gmail.com>
2024-03-28 15:54:50 +0100

    Clean-up visualize-rays script

--- visualize-rays.py
+++ visualize-rays.py
@@ -14,20 +14,22 @@ dt = np.dtype([
     ('bx', np.float32),
     ('by', np.float32),
     ('bz', np.float32),
-    ])
+])
 rays = pd.DataFrame(np.fromfile('/tmp/raylog0.bin', dt))
 print(f'Read {len(rays)} rays...')
-print(rays)
 
-rays = rays[(rays.px >= 670) & (rays.px <= 675) & (rays.py >= 814) & (rays.py <= 820)]
-print(rays)
+pixels = np.array([[100, 150], [350, 450]])
+
+rays = rays[(rays.px >= pixels[0][0]) & (rays.px <= pixels[0][1])
+            & (rays.py >= pixels[1][0]) & (rays.py <= pixels[1][1])]
 
 rerun.init('raytracing')
 rerun.connect()
 rerun.log('rays', rerun.ViewCoordinates.RIGHT_HAND_Y_UP, timeless=True)
 
-for (f, ray) in rays.groupby(['i', 'px', 'py']):
+for (_, ray) in rays.groupby(['i', 'px', 'py']):
     segments = ray[['ax', 'ay', 'az', 'bx', 'by', 'bz']].to_numpy()
     segments = segments.reshape((len(ray), 2, 3))
-    path = f'rays/iter{int(ray.i.iloc[0])}/{int(ray.px.iloc[0])}x{int(ray.py.iloc[0])}'
+    path = f'rays/iter{ray.i.iloc[0]}/{ray.px.iloc[0]}x{ray.py.iloc[0]}'
+    print(path)
     rerun.log(path, rerun.LineStrips3D(segments, radii=0.001), timeless=True)
