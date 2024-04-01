9da6288898b3fc55ede3d9a36a19fbe48eedcd2e
Daniel Oom <daoo314@gmail.com>
2024-04-01 09:51:47 +0200

    Fix compilation

--- raytracer-cli/src/main.rs
+++ raytracer-cli/src/main.rs
@@ -7,12 +7,22 @@ use wavefront::{mtl, obj};
 struct Args {
     #[arg(short, long, required = true)]
     input: std::path::PathBuf,
+
     #[arg(short, long, required = true)]
     output: std::path::PathBuf,
     #[arg(short, long, default_value_t = 512)]
     width: u32,
     #[arg(short, long, default_value_t = 512)]
     height: u32,
+
+    #[arg(long, default_value_t = 20)]
+    max_depth: u32,
+    #[arg(long, default_value_t = 2.0)]
+    traverse_cost: f32,
+    #[arg(long, default_value_t = 1.0)]
+    intersect_cost: f32,
+    #[arg(long, default_value_t = 0.8)]
+    empty_factor: f32,
 }
 
 fn main() {
@@ -24,7 +34,14 @@ fn main() {
     println!("Loading {}...", mtl_path.display());
     let mtl = mtl::mtl(str::from_utf8(&fs::read(mtl_path).unwrap()).unwrap());
     println!("Building scene...");
-    let scene = Scene::from_wavefront(&obj, &mtl);
+    let scene = Scene::from_wavefront(
+        &obj,
+        &mtl,
+        args.max_depth,
+        args.traverse_cost,
+        args.intersect_cost,
+        args.empty_factor,
+    );
     println!("Triangles: {}", scene.triangle_normals.len());
 
     let pinhole = Pinhole::new(&scene.cameras[0], args.width as f32 / args.height as f32);
