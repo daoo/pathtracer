d874a015348945099774427025cc4812fdd77e70
Daniel Oom <daoo314@gmail.com>
2024-07-17 21:02:04 +0200

    Add arguments to kdtree reducer
    * Add --seed argument for repeatable results.
    * Add --fail argument for writing out test ray as binary data.

--- kdtree-reducer-cli/src/main.rs
+++ kdtree-reducer-cli/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufReader, BufWriter},
+    io::{BufReader, BufWriter, Write},
     time::Instant,
 };
 
@@ -44,18 +44,13 @@ fn try_removing(
     verify_removal(ray, actual, &tree).then_some(tree.geometries)
 }
 
-fn reduce_tree(
-    ray: &Ray,
-    geometries: Vec<Geometry>,
-    expected_intersection: &(usize, RayIntersection),
-    actual_intersection: &(usize, RayIntersection),
-) -> KdTree {
-    let actual_geometry = geometries[actual_intersection.0].clone();
-    let actual = (actual_geometry, actual_intersection.1);
+fn reduce_tree(seed: u64, intersection: &CheckedIntersection, geometries: Vec<Geometry>) -> KdTree {
+    let actual_geometry = geometries[intersection.kdtree.0].clone();
+    let actual = (actual_geometry, intersection.kdtree.1);
     let mut geometries = geometries;
-    geometries.swap(0, expected_intersection.0);
-    geometries.swap(1, actual_intersection.0);
-    geometries[2..].shuffle(&mut SmallRng::from_entropy());
+    geometries.swap(0, intersection.reference.0);
+    geometries.swap(1, intersection.kdtree.0);
+    geometries[2..].shuffle(&mut SmallRng::seed_from_u64(seed));
     let mut try_index: usize = 2;
     let mut try_count = geometries.len() - try_index;
     eprintln!(
@@ -67,7 +62,13 @@ fn reduce_tree(
         try_count = try_count.clamp(1, remaining);
         eprint!("  Trying to remove {: <5}", try_count);
         let time_before = Instant::now();
-        let reduced = try_removing(ray, &actual, &geometries, try_index, try_count);
+        let reduced = try_removing(
+            &intersection.ray,
+            &actual,
+            &geometries,
+            try_index,
+            try_count,
+        );
         let duration = Instant::now().duration_since(time_before).as_micros() as f64 / 1000.0;
         if let Some(reduced) = reduced {
             geometries = reduced;
@@ -102,31 +103,81 @@ struct Args {
     /// Output reduced kd-tree JSON data path
     #[arg(short = 'o', long, required = true)]
     output: std::path::PathBuf,
+
+    /// Output ray fail binary data path
+    #[arg(short = 'f', long)]
+    fail: Option<std::path::PathBuf>,
+
+    /// Seed for random generator used to shuffle input geometry
+    #[arg(short = 's', long, required = true)]
+    seed: u64,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct CheckedIntersection {
+    pub ray: Ray,
+    pub reference: (usize, RayIntersection),
+    pub kdtree: (usize, RayIntersection),
+}
+
+impl CheckedIntersection {
+    fn as_bytes(&self, iteration: u16) -> [u8; 50] {
+        let mut bytes = [0u8; 50];
+        let ray = self.ray.extended(self.kdtree.1.t);
+        let correct_point = self.ray.param(self.reference.1.t);
+        let actual_point = self.ray.param(self.kdtree.1.t);
+        bytes[0..2].copy_from_slice(&iteration.to_le_bytes());
+        bytes[2..6].copy_from_slice(&ray.origin.x.to_le_bytes());
+        bytes[6..10].copy_from_slice(&ray.origin.y.to_le_bytes());
+        bytes[10..14].copy_from_slice(&ray.origin.z.to_le_bytes());
+        bytes[14..18].copy_from_slice(&ray.direction.x.to_le_bytes());
+        bytes[18..22].copy_from_slice(&ray.direction.y.to_le_bytes());
+        bytes[22..26].copy_from_slice(&ray.direction.z.to_le_bytes());
+        bytes[26..30].copy_from_slice(&correct_point.x.to_le_bytes());
+        bytes[30..34].copy_from_slice(&correct_point.y.to_le_bytes());
+        bytes[34..38].copy_from_slice(&correct_point.z.to_le_bytes());
+        bytes[38..42].copy_from_slice(&actual_point.x.to_le_bytes());
+        bytes[42..46].copy_from_slice(&actual_point.y.to_le_bytes());
+        bytes[46..50].copy_from_slice(&actual_point.z.to_le_bytes());
+        bytes
+    }
 }
 
 fn main() {
     let args = Args::parse();
 
-    let ray = Ray::new(
-        [3.897963, 0.24242611, -4.203691].into(),
-        [-13.897963, 9.757574, 14.2036915].into(),
-    );
-    let expected_intersection = (
-        7589,
-        RayIntersection {
-            t: 0.0004729527,
-            u: 0.09395919,
-            v: 0.47453666,
-        },
-    );
-    let actual_intersection = (
-        5556,
-        RayIntersection {
-            t: 0.05429069,
-            u: 0.2189177,
-            v: 0.74337834,
-        },
-    );
+    let intersection = CheckedIntersection {
+        ray: Ray::new(
+            [3.897963, 0.24242611, -4.203691].into(),
+            [-13.897963, 9.757574, 14.2036915].into(),
+        ),
+        reference: (
+            7589,
+            RayIntersection {
+                t: 0.0004729527,
+                u: 0.09395919,
+                v: 0.47453666,
+            },
+        ),
+        kdtree: (
+            5556,
+            RayIntersection {
+                t: 0.05429069,
+                u: 0.2189177,
+                v: 0.74337834,
+            },
+        ),
+    };
+    eprintln!("Testing with:");
+    eprintln!("  {:?}", &intersection.ray);
+    eprintln!("  Expected: {:?}", &intersection.reference);
+    eprintln!("    Actual: {:?}", &intersection.kdtree);
+    if let Some(path) = args.fail {
+        eprintln!("Writing test ray to {:?}...", path);
+        let file = File::create(path).unwrap();
+        let mut buf = BufWriter::new(file);
+        buf.write_all(&intersection.as_bytes(1)).unwrap();
+    }
 
     eprintln!("Loading {}...", args.input.display());
     let obj = obj::obj(&mut BufReader::new(File::open(&args.input).unwrap()));
@@ -158,17 +209,7 @@ fn main() {
         .collect::<Vec<_>>();
     eprintln!("  Geometries: {}", geometries.len());
 
-    eprintln!("Testing with:");
-    eprintln!("  {:?}", &ray);
-    eprintln!("  Expected: {:?}", &expected_intersection);
-    eprintln!("    Actual: {:?}", &actual_intersection);
-
-    let tree = reduce_tree(
-        &ray,
-        geometries,
-        &expected_intersection,
-        &actual_intersection,
-    );
+    let tree = reduce_tree(args.seed, &intersection, geometries);
 
     write_tree_json(
         &mut BufWriter::new(File::create(args.output).unwrap()),
