18d9e37fba803d114c8ae650a7fd0f73e4c8ae2a
Daniel Oom <daoo314@gmail.com>
2024-08-05 22:27:54 +0200

    Fix clipping arrayvec capacity issue

--- geometry/src/clip.rs
+++ geometry/src/clip.rs
@@ -6,7 +6,7 @@ use crate::{aabb::Aabb, aap::Aap, ray::Ray};
 /// Clip Triangle against AABB.
 ///
 /// Implements the Sutherland-Hodgman algorithm.
-pub fn clip_triangle_aabb(v0: &Vec3, v1: &Vec3, v2: &Vec3, aabb: &Aabb) -> ArrayVec<Vec3, 6> {
+pub fn clip_triangle_aabb(v0: &Vec3, v1: &Vec3, v2: &Vec3, aabb: &Aabb) -> ArrayVec<Vec3, 8> {
     let aabb_min = aabb.min();
     let aabb_max = aabb.max();
     let clip_planes = [
@@ -26,12 +26,10 @@ pub fn clip_triangle_aabb(v0: &Vec3, v1: &Vec3, v2: &Vec3, aabb: &Aabb) -> Array
         }
     };
 
-    let mut output = ArrayVec::<Vec3, 6>::new();
-    unsafe {
-        output.push_unchecked(*v1);
-        output.push_unchecked(*v2);
-        output.push_unchecked(*v0);
-    }
+    let mut output = ArrayVec::<Vec3, 8>::new();
+    output.push(*v1);
+    output.push(*v2);
+    output.push(*v0);
 
     for clip_plane @ (_, plane) in clip_planes {
         if output.is_empty() {
@@ -45,17 +43,11 @@ pub fn clip_triangle_aabb(v0: &Vec3, v1: &Vec3, v2: &Vec3, aabb: &Aabb) -> Array
             let intersecting = plane.intersect_ray_point(&ray);
             if is_inside(clip_plane, *b) {
                 if !is_inside(clip_plane, *a) {
-                    unsafe {
-                        output.push_unchecked(intersecting.unwrap_unchecked());
-                    }
+                    output.push(intersecting.unwrap());
                 }
-                unsafe {
-                    output.push_unchecked(*b);
-                }
+                output.push(*b);
             } else if is_inside(clip_plane, *a) {
-                unsafe {
-                    output.push_unchecked(intersecting.unwrap_unchecked());
-                }
+                output.push(intersecting.unwrap());
             }
         }
     }
@@ -165,7 +157,7 @@ mod tests {
     }
 
     #[test]
-    fn clip_incorrect_clamping_error() {
+    fn clip_incorrect_clamping_if_points_are_outside_two_or_more_clip_planes() {
         let v0 = Vec3::new(3.835834, 0.136162, -3.724971);
         let v2 = Vec3::new(3.836198, 0.135679, -4.556344);
         let v1 = Vec3::new(3.952836, 0.369915, -4.555017);
@@ -181,6 +173,31 @@ mod tests {
             Vec3::new(3.8884628, 0.241052, -4.272935),
             Vec3::new(3.8883352, 0.241052, -4.0974307),
             Vec3::new(3.901177, 0.2667079, -4.1885333),
+        ];
+        assert_eq!(actual.as_slice(), expected);
+    }
+
+    #[test]
+    fn clip_maximum_array_vec_capacity() {
+        let v0 = Vec3::new(-1.630846, 1.597002, -6.346155);
+        let v1 = Vec3::new(-1.630858, 1.59699, -6.807356);
+        let v2 = Vec3::new(-1.47169, 1.537329, -6.807351);
+        let aabb = Aabb::from_extents(
+            Vec3::new(-1.639749, 1.5598166, -6.807353),
+            Vec3::new(-1.531684, 1.5970019, -6.3492346),
+        );
+
+        let actual = clip_triangle_aabb(&v0, &v1, &v2, &aabb);
+
+        let expected = [
+            Vec3::new(-1.630846, 1.5970019, -6.3492346),
+            Vec3::new(-1.6308461, 1.5970019, -6.3507214),
+            Vec3::new(-1.630858, 1.59699, -6.807353),
+            Vec3::new(-1.5316842, 1.5598166, -6.807353),
+            Vec3::new(-1.5316842, 1.5598166, -6.807353),
+            Vec3::new(-1.531684, 1.5598166, -6.8061028),
+            Vec3::new(-1.531684, 1.5598228, -6.633503),
+            Vec3::new(-1.6297833, 1.5966036, -6.3492346),
         ];
         assert_eq!(actual.as_slice(), expected);
     }
